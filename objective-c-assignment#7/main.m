4f89ba2141b09b521b3d292b7194f0adfca9367a
aslancll <celalaslan@outlook.com>
2018-06-23 16:17:38 -0700

    Initial Commit

---
+++ objective-c-assignment#7/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objective-c-assignment#7
+//
+//  Created by Celal Aslan on 2018-06-23.
+//  Copyright © 2018 Celal Aslan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
